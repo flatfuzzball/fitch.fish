59d480774835083841fc72e668df175110999173
flat <127520563+flatfuzzball@users.noreply.github.com>
2024-01-08 19:42:33 +0100

    Create fitch.fish

---
+++ completions/fitch.fish
@@ -0,0 +1,4 @@
+complete -c fitch -x -l get -d "Install a plugin"
+complete -c fitch -x -l del -d "Remove a plugin"
+complete -c fitch -x -a -g -d "Install a plugin"
+complete -c fitch -x -a -d -d "Remove a plugin"
